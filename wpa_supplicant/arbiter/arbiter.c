af02170ad05a97f2b4a2caea3d97f09aa4d2a1d9
Further Lee <95588lsj@gmail.com>
2012-08-30 11:23:39 +0800

    add arbiter_init

--- wpa_supplicant/arbiter/arbiter.c
+++ wpa_supplicant/arbiter/arbiter.c
@@ -4,8 +4,16 @@
 #include "arbiter.h"
 
 arbiter *arbiter_init(struct wpa_supplicant *wpa_s){
-  // TODO
-  return NULL;
+  arbiter* res = (arbiter *)os_malloc(sizeof(arbiter));
+  if (res == NULL)
+    return NULL;
+
+  res->state = ARBITER_IDLE;
+  res->wpa_s = wpa_s;
+  res->filter_num = 2;
+  res->filters[0] = consortium_filter;
+  res->filters[1] = random_filter;
+  return res;
 }
 
 void arbiter_deinit(arbiter *arb){
